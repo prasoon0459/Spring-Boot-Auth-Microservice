8b542bd203dd08b6eea73983d6f6a52254f81925
prasoon0459 <prasoon040999@gmail.com>
2020-06-12 23:44:23 +0530

    updated README

--- src/main/java/com/example/authApp/web/AuthController.java
+++ src/main/java/com/example/authApp/web/AuthController.java
@@ -5,6 +5,7 @@ import static org.springframework.http.ResponseEntity.ok;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
